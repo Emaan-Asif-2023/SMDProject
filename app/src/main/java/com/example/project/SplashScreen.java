2867d2539243d1c25fad3fd2618a5b856772416f
Emaan-Asif-2023 <emaan.asifsheikh2004@gmail.com>
2026-04-22 22:48:28 +0500

    Splash Screen done

---
+++ app/src/main/java/com/example/project/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.example.project;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView tvName;
+    ImageView ivLogo;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tvName=findViewById(R.id.tvName);
+        ivLogo=findViewById(R.id.ivLogo);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.animation);
+        ivLogo.startAnimation(anim);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, Signup.class));
+            finish();
+        }, 5000);
+
+
+
+    }
+}
